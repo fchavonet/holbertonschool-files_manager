5f6bb057a75eb12ee34cd1415031562a2ec9d4a9
fchavonet <fchavonet@mail.fr>
2025-06-15 19:14:56 +0200

    Fix: ESLint error (replace " by ' and tabs by spaces).

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,63 +1,63 @@
-import redis from "redis";
+import redis from 'redis';
 
 class RedisClient {
-	constructor() {
-		// Create a new Redis client instance.
-		this.client = redis.createClient();
+  constructor() {
+    // Create a new Redis client instance.
+    this.client = redis.createClient();
 
-		// Log any connection errors to the console.
-		this.client.on("error", (error) => {
-			console.error("Redis client error: " + error);
-		});
-	}
+    // Log any connection errors to the console.
+    this.client.on('error', (error) => {
+      console.error('Redis client error:', error);
+    });
+  }
 
-	// Check if the Redis client is currently connected.
-	isAlive() {
-		if (this.client.connected) {
-			return true;
-		}
+  // Check if the Redis client is currently connected.
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
 
-		return false;
-	}
+    return false;
+  }
 
-	// Retrieve a value from Redis by key.
-	async get(key) {
-		return new Promise((resolve, reject) => {
-			this.client.get(key, (error, value) => {
-				if (error) {
-					return reject(error);
-				}
+  // Retrieve a value from Redis by key.
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, value) => {
+        if (error) {
+          return reject(error);
+        }
 
-				return resolve(value);
-			});
-		});
-	}
+        return resolve(value);
+      });
+    });
+  }
 
-	// Store a value in Redis with an expiration time.
-	async set(key, value, duration) {
-		return new Promise((resolve, reject) => {
-			this.client.setex(key, duration, value, (error, reply) => {
-				if (error) {
-					return reject(error);
-				}
+  // Store a value in Redis with an expiration time.
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (error, reply) => {
+        if (error) {
+          return reject(error);
+        }
 
-				return resolve(reply);
-			});
-		});
-	}
+        return resolve(reply);
+      });
+    });
+  }
 
-	// Delete a key from Redis.
-	async del(key) {
-		return new Promise((resolve, reject) => {
-			this.client.del(key, (error, reply) => {
-				if (error) {
-					return reject(err);
-				}
+  // Delete a key from Redis.
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, reply) => {
+        if (error) {
+          return reject(error);
+        }
 
-				return resolve(reply);
-			});
-		});
-	}
+        return resolve(reply);
+      });
+    });
+  }
 }
 
 // Export a singleton instance of RedisClient for reuse.
