bc737080d313578923c4b1b93a49632201072a24
fchavonet <fchavonet@mail.fr>
2025-06-17 09:22:41 +0200

    Fix: ESLint error (fucking spaces in place of tabs).

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,18 +2,18 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-	static getStatus(req, res) {
-		return res.status(200).json({
-			redis: redisClient.isAlive(),
-			db: dbClient.isAlive(),
-		});
-	}
+  static getStatus(req, res) {
+    return res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
 
-	static async getStats(req, res) {
-		const users = await dbClient.nbUsers();
-		const files = await dbClient.nbFiles();
-		return res.status(200).json({ users, files });
-	}
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    return res.status(200).json({ users, files });
+  }
 }
 
 export default AppController;
